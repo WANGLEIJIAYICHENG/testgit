7abec2e3b6fed3cdcfbd6eae45f685e500397578
Maker <1003964217@qq.com>
2020-02-08 21:31:09 +0800

    胡江浩修改

--- helloword/Program.cs
+++ helloword/Program.cs
@@ -6,7 +6,7 @@ namespace helloword
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!hahahhahahh");
         }
     }
 }
